c1abc575ad4901ea1a9d2b2b562f909df6402d5f
JayKrishnareddy <jaykrishnareddy@gmail.com>
2022-08-29 16:00:28 +0200

    Updated

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ var employees = []Employee{
 	},
 }
 
-// Fetch all Employes Data
+// Fetch all Employes Data from the database
 func getEmployees(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, employees)
 }
